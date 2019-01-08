718e6f67488e1200731034b7d9c26e2becce4c59
Frank Bauer <developer@ambertation.de>
2019-01-08 15:40:30 +0100

    calendar tests

--- testenv/test.js
+++ testenv/test.js
@@ -1,50 +1,65 @@
 const suncalc = require('suncalc'),
-      moment = require('moment'),
-      columnify = require('columnify');
+    moment = require('moment'),
+    columnify = require('columnify'),
+    https = require('https'),
+    fs = require('fs'),
+    icalExpander = require('ical-expander'),
+    xmlParser = require('xml-js'),
+    ical = require('../lib/iCal.js');
 
 const sampleConfig = require('./sampleConfig.json');
-function nulllog(msg){
+
+function nulllog(msg) {
     console.log(msg);
 };
 
 
-console.log(sampleConfig);
-console.stime = function(date) {
+console.stime = function (date) {
     return moment(date).format('LTS \t\t\t ll');
 }
-console.time = function(date) {
+console.time = function (date) {
     console.log(console.stime(date));
 }
 
 const pseudoHomebridge = {
     version: "TEST DUMMY",
-    hap:{
+    hap: {
         Service: require("hap-nodejs").Service,
         Characteristic: require("hap-nodejs").Characteristic,
-        uuid:0
+        uuid: 0
     },
-    platformAccessory:[],
-    registerPlatform : function(pluginName, platformName, constructor, dynamic){
-        const obj = new constructor(nulllog, sampleConfig);    
-        
+    platformAccessory: [],
+    registerPlatform: function (pluginName, platformName, constructor, dynamic) {
+        const obj = new constructor(nulllog, sampleConfig);
+
         let events = obj.eventsForDate(new Date(), false);
         console.log("TODAY's EVENT LIST");
-        console.logEvents(obj.events); 
+        console.logEvents(obj.events);
         console.log("CURRENT")
         console.log(obj.currentEvent, obj.posForTime(new Date()).altitude * 180 / Math.PI);
-        
+
         obj.updateState();
     },
-    
+    registerAccessory: function (pluginName, platformName, constructor, dynamic) {
+        const obj = new constructor(nulllog, sampleConfig);
+
+        let events = obj.eventsForDate(new Date(), false);
+        console.log("TODAY's EVENT LIST");
+        console.logEvents(obj.events);
+        console.log("CURRENT")
+        console.log(obj.currentEvent, obj.posForTime(new Date()).altitude * 180 / Math.PI);
+
+        obj.updateState();
+    },
+
 };
-const plugin = require('../index.js')(pseudoHomebridge);
+//const plugin = require('../index.js')(pseudoHomebridge);
 
+const username = "",
+      password = "",
+      url = "";
 
-
-
-
-
-
-
-
-
+ical.loadEventsForDay(moment(), {url:url, username:username, password:password, type:'caldav'}, (list, start, end) => {
+    //console.log(list);
+    ical.eventsAt(moment(), list).map(e => console.log(e));
+});
