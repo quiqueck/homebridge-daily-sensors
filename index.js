1f5cb01599fcfddd212e48abf7e4e24560467bc1
Frank Bauer <developer@ambertation.de>
2019-10-07 18:59:10 +0200

    Customized Types

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ module.exports = function(homebridge) {
 
     web.setCharacteristics(Characteristic);
 
+    const CustomTypes = require('./lib/CustomTypes.js')(Service, Characteristic, UUIDGen);
     const DailySensor = require('./lib/Sensor.js')(Service, Characteristic).DailySensor;
     const DailyGroup = require('./lib/Group.js')(Service, Characteristic, DailySensor).DailyGroup;
-    const DailySocket = require('./lib/DailySocket.js')(Service, Characteristic).DailySocket;
-    const DailyPlatform = require('./lib/Platform.js')(Service, Characteristic, Accessory, UUIDGen, DailySensor, DailyGroup, DailySocket).DailyPlatform;
+    const DailySocket = require('./lib/DailySocket.js')(Service, Characteristic, CustomTypes);
+    const DailyPlatform = require('./lib/Platform.js')(Service, Characteristic, Accessory, UUIDGen, DailySensor, DailyGroup, DailySocket.DailySocket, DailySocket.DailyLight).DailyPlatform;
     //console.log(DailySensor, DailyGroup)
     homebridge.registerAccessory("homebridge-daily-sensors", "DailySensors", DailySensor);
     homebridge.registerAccessory("homebridge-daily-sensors", "DailyGroup", DailyGroup);
-    homebridge.registerAccessory("homebridge-daily-sensors", "DailySocket", DailySocket);
+    homebridge.registerAccessory("homebridge-daily-sensors", "DailySocket", DailySocket.DailySocket);
+    homebridge.registerAccessory("homebridge-daily-sensors", "DailyLight", DailySocket.DailyLight);
     homebridge.registerPlatform("homebridge-daily-sensors", "DailyPlatform", DailyPlatform, true);
 }
